9c83bf79c34106c44c75595e01089fa3d8039a38
Michael Gale <m.gale@warwick.ac.uk>
2019-10-11 13:48:34 +0100

    Add examples of real algorithms (in Java)

---
+++ Hash.java
@@ -0,0 +1,35 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash {
+    private static String toHex(byte[] buffer) {
+        StringBuffer sb = new StringBuffer();
+		for (byte b : buffer) {
+			sb.append(String.format("%02x", b & 0xff));
+        }
+        
+        return sb.toString();
+    }
+
+    public static String hash(String algorithm, String text) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        md.update(text.getBytes());
+        byte[] digest = md.digest();
+        
+        return toHex(digest);
+    }
+
+    public static void main(String[] args) {
+        String text = "Cake";
+
+        try {
+            System.out.printf("md5(\"%s\")=\"%s\"\n", text, hash("MD5", text));
+            System.out.printf("sha1(\"%s\")=\"%s\"\n", text, hash("SHA-1", text));
+            System.out.printf("sha256(\"%s\")=\"%s\"\n", text, hash("SHA-256", text));
+            System.out.printf("sha512(\"%s\")=\"%s\"\n", text, hash("SHA-512", text));
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
