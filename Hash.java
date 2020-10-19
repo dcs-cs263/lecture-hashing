fc38ad4fcf0171aa8b1b390e9c5c6b99ac7691e4
Michael Gale <m.gale@warwick.ac.uk>
2020-10-19 15:20:27 +0100

    Make Java example interactive

--- Hash.java
+++ Hash.java
@@ -20,16 +20,24 @@ public class Hash {
     }
 
     public static void main(String[] args) {
-        String text = "Cake";
+        while(true) {
+            System.out.print("Enter password: ");
+            String text = System.console().readLine();
 
-        try {
-            System.out.printf("md5(\"%s\")=\"%s\"\n", text, hash("MD5", text));
-            System.out.printf("sha1(\"%s\")=\"%s\"\n", text, hash("SHA-1", text));
-            System.out.printf("sha256(\"%s\")=\"%s\"\n", text, hash("SHA-256", text));
-            System.out.printf("sha512(\"%s\")=\"%s\"\n", text, hash("SHA-512", text));
+            System.out.println();
+
+            try {
+                System.out.printf("md5(\"%s\")=\"%s\"\n", text, hash("MD5", text));
+                System.out.printf("sha1(\"%s\")=\"%s\"\n", text, hash("SHA-1", text));
+                System.out.printf("sha256(\"%s\")=\"%s\"\n", text, hash("SHA-256", text));
+                System.out.printf("sha512(\"%s\")=\"%s\"\n", text, hash("SHA-512", text));
+            }
+            catch(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            System.out.println();
         }
-        catch(Exception ex) {
-            ex.printStackTrace();
-        }
+        
     }
 }
